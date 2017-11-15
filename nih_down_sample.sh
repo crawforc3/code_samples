1468c8755d2a72fce431968ea3002156fefeae82
crawforc3 <crawforc3@gmail.com>
2017-11-15 11:13:16 -0800

    Update nih_down_sample.sh

--- nih_down_sample.sh
+++ nih_down_sample.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Script to down sample NIH X-Ray dataset at https://www.kaggle.com/nih/xray1-2  [3-4, 5-6, 7-8, 9-10, 11-12]
 # Randomly samples 5% of images from the 12 zip files
 
@@ -21,10 +20,25 @@ function extract_sample_size_names(){
   unzip -l "$file_name" | grep png | shuf -n "$sample_size" | sed 's/   /,/g' | cut -d , -f 3
 }
 
+function create_zip(){
+  zip sample.zip images/*
+}
+
+function create_labels(){
+  # Find matching file names in $temp_file and $all_labels and create sample_labels.csv
+  all_labels=$1
+  temp_file=temp.txt
+  ls -1 images > $temp_file
+  awk -F, 'NR==FNR {a[$0];next} $1 in a' $temp_file $all_labels > sample_labels.csv'
+  rm $temp_file
+}
 
 for file in *.zip; do
   num_files=$(get_file_count $file)
   sample_size=$(get_sample_size $num_files)
   unzip $file $(extract_sample_size_names $file $sample_size)
+  create_zip
+  create_labels Data_Entry_2017.csv 
 done
+
 
