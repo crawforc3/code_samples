eac2a454324a64dc319a6ab1c18558575404c7ce
crawforc3 <crawforc3@gmail.com>
2017-11-15 09:08:25 -0800

    Add down_sample.sh

---
+++ down_sample.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function get_file_count(){
+  file_name=$1
+  # Given a zip file name return a count of the files inside
+  unzip -l "$file_name" | grep -c png
+}
+
+function get_sample_size(){
+  # Given number of files calculate the sample size (5% of all files)
+  python -c "print(round($1 * 0.05))"
+}
+
+function extract_sample_size_names(){
+  file_name=$1
+  sample_size=$2
+  # Given a zip file and sample size, create a random list of file names in the zip file
+  unzip -l "$file_name" | grep png | shuf -n "$sample_size" | sed 's/   /,/g' | cut -d , -f 3
+}
+
+
+for file in *.zip; do
+  num_files=$(get_file_count $file)
+  sample_size=$(get_sample_size $num_files)
+  unzip $file $(extract_sample_size_names $file $sample_size)
+done
+
