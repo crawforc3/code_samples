b2d3570be722867d65bfb55021f376a629ec947a
crawforc3 <crawforc3@gmail.com>
2018-01-24 14:16:49 -0800

    Add changes to use tar files

--- nih_down_sample.sh
+++ nih_down_sample.sh
@@ -7,12 +7,17 @@ function get_file_count(){
   # Given a zip file name return a count of the files inside
   # tar -tf "$file_name" | grep -c png
   unzip -l "$file_name" | grep -c png
-  
+}
+
+function get_file_count_tar(){
+  file_name=$1
+  # Given a tar file name return a count of the files inside
+  tar -tf "$file_name" | grep -c png
 }
 
 function get_sample_size(){
   # Given number of files calculate the sample size (5% of all files)
-  python -c "print(round($1 * 0.05))"
+  python -c "from __future__ import print_function; print(int(round(100 * 0.05)))"
 }
 
 function extract_sample_size_names(){
@@ -21,6 +26,13 @@ function extract_sample_size_names(){
   # Given a zip file and sample size, create a random list of file names from the zip file
   # tar -tf "$file_name" | grep png | shuf -n "$sample_size" | sed 's/   /,/g' | cut -d , -f 3
   unzip -l "$file_name" | grep png | shuf -n "$sample_size" | sed 's/   /,/g' | cut -d , -f 3
+}
+
+function extract_sample_size_names_tar(){
+  file_name=$1
+  sample_size=$2
+  # Given a zip file and sample size, create a random list of file names from the zip file
+  tar -tf "$file_name" | grep png | shuf -n "$sample_size" | sed 's/   /,/g' | cut -d , -f 3
 }
 
 function create_zip(){
@@ -40,12 +52,23 @@ function create_labels(){
   rm $temp_file
 }
 
-for zip in *.zip; do
-# for tar in *tar.gz
-  num_files=$(get_file_count $zip)
+# Use this if images archives are .zip
+for file in *.zip; do
+  num_files=$(get_file_count $file)
   sample_size=$(get_sample_size $num_files)
-  unzip $zip $(extract_sample_size_names $zip $sample_size)
+  unzip $file $(extract_sample_size_names $file $sample_size)
+  #tar $file $(extract_sample_size_names $file $sample_size)
   # create_tar
   create_zip
   create_labels Data_Entry_2017.csv 
 done
+
+
+# Use this if images archives are .tar.gz
+#for file in *tar.gz
+  #num_files=$(get_file_count_tar $file)
+  #sample_size=$(get_sample_size $num_files)
+  #tar $file $(extract_sample_size_names_tar $file $sample_size)
+  #create_tar
+  #create_labels Data_Entry_2017.csv 
+#done
